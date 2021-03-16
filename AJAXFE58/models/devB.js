c6a0c199a13324f7dd046f0c707f23670424511a
Viet Anh <doanhviet96@gmail.com>
2021-03-16 09:53:35 +0700

    Ajax

---
+++ AJAXFE58/models/devB.js
@@ -0,0 +1,10 @@
+var devB = function () {
+    this.ma = '';
+    this.hoTen = '';
+    this.hienThiThongTin = function (){
+        console.log('devB');
+    }
+}
+
+//git merge [tenNhanh cần gộp code tại local]
+//git pull origin [tenNhanh cần gộp code tại server]
